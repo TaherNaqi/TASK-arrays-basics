997e010edc27fa1f7cf39054027135d351ec0d96
TaherNaqi <eng-naqi@hotmail.com>
2021-12-23 10:32:31 +0300

    Refining the code

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ console.log(hobbies[hobbies.length - 1]);
 hobbies.push("doing nothing");
 //removing last two elements in the array:
 console.log(hobbies[hobbies.length - 1]);
-hobbies.pop(hobbies[hobbies.length - 1]);
-hobbies.pop([hobbies.length - 1]);
+hobbies.pop(hobbies.length - 1);
+hobbies.pop(hobbies.length - 1);
 console.log(hobbies.length);
 //Emptying Array:
 while (hobbies.length > 0) {
